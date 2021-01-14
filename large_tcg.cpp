362dd70e43799cb9de544c37c2acfb5606eb7bc8
Sumanth Benhur <sbenhur2@gmail.com>
2021-01-14 13:46:25 +0530

    lg tcg

---
+++ large_tcg.cpp
@@ -0,0 +1,18 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main()
+{
+    freopen("input-file.txt", "w", stdout);
+    long long int n = 1000000;
+
+    cout << n << "\n";
+
+    for (auto i = 1; i <= n; ++i)
+    {
+        cout << 1 << " ";
+    }
+
+    return 0;
+}
